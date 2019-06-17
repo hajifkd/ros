2a9368315c4e224f98355e81361cb52c1ee607ef
hfukuda <hajime.fukuda@me.com>
2019-06-17 22:38:13 +0900

    added zero division

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -6,6 +6,7 @@ lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
         idt.breakpoint.set_handler_fn(breakpoint_handler);
+        idt.divide_by_zero.set_handler_fn(zero_division_handler);
         idt
     };
 }
@@ -16,4 +17,8 @@ pub fn init_idt() {
 
 extern "x86-interrupt" fn breakpoint_handler(stack_frame: &mut InterruptStackFrame) {
     println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
+}
+
+extern "x86-interrupt" fn zero_division_handler(stack_frame: &mut InterruptStackFrame) {
+    println!("EXCEPTION: ZERO DIVISION\n{:#?}", stack_frame);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
+#![feature(core_intrinsics)]
 #![test_runner(ros::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
@@ -11,6 +12,11 @@ pub extern "C" fn _start() -> ! {
     init();
 
     println!("Hello, world!");
+    // x86_64::instructions::interrupts::int3();
+    unsafe {
+        let i = core::intrinsics::unchecked_div(1, 0);
+        println!("Zero division result: {}", i);
+    }
     x86_64::instructions::interrupts::int3();
     println!("こんにちは、世界!");
 
